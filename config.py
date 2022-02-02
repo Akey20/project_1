f7f9b733d2bea5eb8fd72f4853b8f4c9ce4bc2c8
MarkusShipley <mrksshipley@gmail.com>
2022-02-01 21:04:45 -0500

    csv updated

---
+++ config.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "0a709eafcf186af1422bb585c0d77225"
+
+# Google API Key
+g_key = "AIzaSyBvumybtTjqDhyzQpHeaxchsZ3zcdHChB4"
+
+#US Census Key
+api_key = "896b429f30b0da1279266699b244a1df521c5359"
